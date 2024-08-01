86299ac1d9465ce3566f88a608c82488fb2ad985
Orlando Azuara <orlando_azu@MacBook-Pro-27.local>
2024-08-01 01:22:05 -0400

    working animations

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 
-onload = () => {
+onclick = () => {
+
+  let audio = new Audio("IWantYou.mp3");
+  audio.play();
+  let playbtn = document.getElementById('playbtn');
+  playbtn.style.display = "none";
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
     clearTimeout(c);
